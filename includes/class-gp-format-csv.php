a252ca6b1db8cffa9d50b0a42ef1d4e3cccae513
pedro-mendonca <ped.gaspar@gmail.com>
2022-11-21 13:00:18 +0000

    Update error log wording

--- includes/class-gp-format-csv.php
+++ includes/class-gp-format-csv.php
@@ -305,7 +305,7 @@ class GP_Format_CSV extends GP_Format {
 					error_log( // phpcs:ignore.
 						wp_sprintf(
 							/* translators: 1: Total header columns. 2: Total row columns. */
-							esc_html__( 'An error occurred while importing the CSV file: The header have %1$d columms, and the rows have %2$s.', 'gp-format-csv' ),
+							esc_html__( 'An error occurred while importing the CSV file: The file header has %1$d columms, and the rows have %2$d.', 'gp-format-csv' ),
 							count( $header ),
 							count( $row )
 						)
@@ -450,7 +450,7 @@ class GP_Format_CSV extends GP_Format {
 				error_log( // phpcs:ignore.
 					wp_sprintf(
 						/* translators: List of invalid column headers. */
-						esc_html__( 'An error occurred while importing the CSV file: Invalid column headers (%l)', 'gp-format-csv' ),
+						esc_html__( 'An error occurred while importing the CSV file: Invalid column headers found (%l)', 'gp-format-csv' ),
 						$errors
 					)
 				);
